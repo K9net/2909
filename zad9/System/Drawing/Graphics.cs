7062606f63afc14d55b7ba6f81c2b42fa211a13b
Valeriy V <73215304+K9net@users.noreply.github.com>
2021-10-11 13:28:55 +0300

    Add files via upload

---
+++ zad9/System/Drawing/Graphics.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Drawing
+{
+    internal class Graphics
+    {
+        public float DpiX { get; internal set; }
+        public float DpiY { get; internal set; }
+
+        internal static Graphics FromHwnd(IntPtr windowHandle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
